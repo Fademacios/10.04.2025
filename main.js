3883423b49ffda03c296eda3b9166df4618f207b
Fademacios <122602505+Fademacios@users.noreply.github.com>
2025-04-10 10:14:01 +0200

    ko

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
-    const naglowek = document.getElementById("przycisk1")
+    const naglowek = document.querySelector("h1)
     const przyciskZmienn = document.getElementById("#przycisk1")
 
     przyciskZmienn.addEventListener("click", function(){
